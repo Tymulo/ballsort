aaca390a664a958760180f988e9fb2d66486aef8
Tymulo <tymkozlow@gmail.com>
2024-06-04 21:54:50 +0200

    test 2

--- sortowanie kul.py
+++ sortowanie kul.py
@@ -57,13 +57,14 @@ def najm_pkt(T,lista_kolorow):
         
 if __name__ == "__main__":
     nazwa = "kule.txt"
-    max_dlg_kol
     ilosc_kolumn(nazwa)
-    print(wczytaj_do1listy(nazwa))
-    print(ilosc_rodz_kul(wczytaj_do1listy(nazwa)))
-    i =1
     kolumna = wczytywanie_kolumny(i)
+    print(wczytaj_do1listy(nazwa))
+    b =(ilosc_rodz_kul(wczytaj_do1listy(nazwa)))
+    print(b)
+    i = 1
+    max_dlg_kol(kolumna)
     print(wczytywanie_kolumny(i))
     print(sprawdzanie_czy_kol_rozwiazana(kolumna))
-    #pkt_koloru(wczytaj_do1listy(nazwa),)
+    pkt_koloru(wczytaj_do1listy(nazwa),b,)
     #test
