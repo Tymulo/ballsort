f604770e37087202e7c39a5dff858f04108954fe
Tymulo <tymkozlow@gmail.com>
2024-08-16 21:57:29 +0200

    improved empty_col_color function, introduced correct data

--- sortowanie_kul.py
+++ sortowanie_kul.py
@@ -20,14 +20,27 @@ def balls_colors(ball_pos):
     return color_list
 
 
-def column_solved(column):
-    if len(balls_colors(column)) == 1:
-        print("column solved")
+def near_to_solve(column, mxcllght):
+    color_list = []
+    for j in column:
+        if j not in color_list:
+            color_list.append(j)
+    if len(color_list) == 1 and len(column) < mxcllght:
         return True
     else:
-        print("column not solved")
         return False
 
+
+def column_solved(column, mxcllght):
+    color_list = []
+    for j in column:
+        if j not in color_list:
+            color_list.append(j)
+    if len(color_list) == 1 and len(column) == mxcllght:
+        return True
+    else:
+        return False
+    
 
 def column_empty(column):
     if len(column) == 0:
@@ -53,36 +66,68 @@ def balls_points(ball_pos, color_list):
     return points
 
 
-def least_pts(points, color_list):
-    lcost = (min(points))
-    pts_index = points.index(lcost)
+def higst_pts(points, color_list):
+    mcost = (max(points))
+    pts_index = points.index(mcost)
     ball_color = color_list[pts_index]
     return ball_color
 
 
-def fill_empty_col(pos_of_balls, lst_pts_ball):
-    for column in pos_of_balls:
-        if column_empty(column) is True:
-            empty_col = column
-            for column in pos_of_balls:
-                if column != empty_col:
-                    if column[0] == lst_pts_ball:
-                        empty_col.append(column[0])
-                        del (column[0])
+def empty_col_color(pos_of_balls, mxcllght, color_list):
+    for a in range(len(color_list)):
+        crpts = balls_points(pos_of_balls, color_list)
+        ball_color = higst_pts(crpts, color_list)
+        for eidx, empty_col in enumerate(pos_of_balls):
+            if column_empty(empty_col) is True:
+                for cidx, column in enumerate(pos_of_balls):
+                    while len(column) > 0 and column[-1] == ball_color and cidx != eidx:
+                        if near_to_solve(column, mxcllght) is True:
+                            break
+                        print("column ", cidx, " to column ", eidx)
+                        empty_col.append(column[-1])
+                        del (column[-1])
+        if len(crpts) > 1:
+            hptsidx = (color_list.index(ball_color))
+            del(crpts[hptsidx], color_list[hptsidx])
+            crpts = balls_points(pos_of_balls, color_list)
+            ball_color = higst_pts(crpts, color_list)
     return pos_of_balls
+
+
+# def sorting_balls(pos_of_balls, mxcllght, color_list):
+#     for a in range(len(color_list)):
+#         crpts = balls_points(pos_of_balls, color_list)
+#         ball_color = higst_pts(crpts, color_list)
+#         for ntslvidx, ntslv in enumerate(pos_of_balls):
+#             print(near_to_solve(ntslv, mxcllght), ntslv[-1], ball_color )
+#             if near_to_solve(ntslv, mxcllght) is True and ntslv[-1] == ball_color:
+#                 for cidx, column in enumerate(pos_of_balls):
+#                     while len(column) > 0 and column[-1] == ball_color and cidx != ntslvidx:
+#                         print("column ", cidx, " to column ", ntslvidx)
+#                         ntslv.append(column[-1])
+#                         del (column[-1])
+#         if len(crpts) > 1:
+#             hptsidx = (color_list.index(ball_color))
+#             del(crpts[hptsidx], color_list[hptsidx])
+#             crpts = balls_points(pos_of_balls, color_list)
+#             ball_color = higst_pts(crpts, color_list)
+#     return pos_of_balls
 
 
 if __name__ == "__main__":
     file_name = "kule.txt"
     pos_of_balls = (ball_pos(file_name))
     print(pos_of_balls)
-    colors = balls_colors(pos_of_balls)
-    print(colors)
-    mxcllght = max_col_lght(pos_of_balls, colors)
+    color_list = balls_colors(pos_of_balls)
+    print(color_list)
+    mxcllght = max_col_lght(pos_of_balls, color_list)
     print(mxcllght)
-    points = balls_points(pos_of_balls, colors)
+    points = balls_points(pos_of_balls, color_list)
     print(points)
-    lst_pts = least_pts(points, colors)
-    print(lst_pts)
-    empty = fill_empty_col(pos_of_balls, lst_pts)
+    hig_pts = higst_pts(points, color_list)
+    print(hig_pts)
+    empty = empty_col_color(pos_of_balls, mxcllght, color_list)
     print(empty)
+    # srt_blls = sorting_balls(pos_of_balls, mxcllght, color_list)
+    # print(srt_blls)
+    
