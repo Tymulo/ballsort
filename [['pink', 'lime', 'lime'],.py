cfd16ec7a8cf0a6399a3ddae6d6384a03e307ad0
Tymulo <tymkozlow@gmail.com>
2025-05-31 14:04:44 +0200

    update

---
+++ [['pink', 'lime', 'lime'],.py
@@ -0,0 +1,51 @@
+[['pink', 'lime', 'lime'], 
+ ['pink', 'pink', 'pink'], 
+ ['lime', 'lime', 'pink', 'green', 'green'], 
+ ['orange', 'purple', 'purple', 'purple', 'purple'], 
+ ['blue', 'blue', 'blue', 'blue', 'blue'], 
+ ['green', 'lime', 'green', 'green'], 
+ ['light_blue', 'light_blue', 'orange', 'light_blue', 'light_blue'], 
+ ['light_blue', 'purple'], 
+ ['gray', 'gray', 'orange', 'orange', 'orange'], 
+ ['red', 'red', 'red', 'red', 'red'], 
+ ['gray', 'gray', 'gray']]
+
+
+[['pink', 'lime', 'blue', 'blue'], 
+ ['pink', 'pink', 'red', 'blue', 'green'], 
+ ['lime', 'lime', 'pink', 'green', 'pink'], 
+ ['orange', 'purple', 'purple', 'orange', 'orange'], 
+ ['blue', 'green', 'purple','light_blue'], 
+ ['green', 'lime', 'green', 'blue'], 
+ ['light_blue', 'light_blue', 'orange', 'light_blue', ], 
+ ['light_blue', 'purple', 'red', 'red', 'lime'], 
+ ['gray', 'gray', 'orange', 'purple', ], 
+ ['red', 'red'], 
+ ['gray', 'gray', 'gray']]
+
+[['pink', 'lime', 'blue','blue' , ], 
+ ['pink', 'pink', 'red', 'blue', 'green'], 
+ ['lime', 'lime', 'pink', 'green', 'pink'], 
+ ['orange', 'purple', 'purple', 'orange', ], 
+ ['blue', 'green', 'light_blue', 'purple', ], 
+ ['green', 'lime', 'green', 'blue', 'orange'], 
+ ['light_blue', 'light_blue', 'orange', 'light_blue', ], 
+ ['light_blue', 'purple', 'red', 'red', 'lime'], 
+ ['gray', 'gray', 'orange', 'purple', ], 
+ ['red''red'], 
+ ['gray', 'gray''gray']]
+
+
+new
+
+[['pink', 'lime', 'blue', 'blue'], 
+ ['pink'], 
+ ['lime', 'lime', 'pink', 'pink', 'pink'], 
+ ['orange', 'purple', 'purple', 'orange', 'orange'], 
+ ['blue', 'green', 'green', 'green'], 
+ ['green', 'lime', 'green', 'blue', 'blue'], 
+ ['light_blue', 'light_blue', 'orange', 'light_blue', 'light_blue'], 
+ ['light_blue', 'purple', 'red', 'red', 'lime'], 
+ ['gray', 'gray', 'orange', 'purple', 'purple'], 
+ ['gray', 'gray', 'gray'], 
+ ['red', 'red', 'red']]
--- sortowanie_kul.py
+++ sortowanie_kul.py
@@ -72,6 +72,20 @@ def higst_pts(points, color_list):
     ball_color = color_list[pts_index]
     return ball_color
 
+def lwst_pts(points, color_list):
+    mcost = (min(points))
+    pts_index = points.index(mcost)
+    ball_color = color_list[pts_index]
+    return ball_color
+
+def pos_solved(pos_of_balls, mxcllght):
+    for column in pos_of_balls:
+        if column_solved(column, mxcllght) or len(column) == 0:
+            continue
+        else:
+            return False
+    return True
+
 
 def empty_col_color(pos_of_balls, mxcllght, color_list):
     for a in range(len(color_list)):
@@ -93,41 +107,137 @@ def empty_col_color(pos_of_balls, mxcllght, color_list):
             ball_color = higst_pts(crpts, color_list)
     return pos_of_balls
 
-4r2tfggt53y
-# def sorting_balls(pos_of_balls, mxcllght, color_list):
+
+# def filling_columns(pos_of_balls, mxcllght, color_list):
 #     for a in range(len(color_list)):
-#         crpts = balls_points(pos_of_balls, color_list)
-#         ball_color = higst_pts(crpts, color_list)
 #         for ntslvidx, ntslv in enumerate(pos_of_balls):
-#             print(near_to_solve(ntslv, mxcllght), ntslv[-1], ball_color )
-#             if near_to_solve(ntslv, mxcllght) is True and ntslv[-1] == ball_color:
+#             if near_to_solve(ntslv, mxcllght) is True:
+#                 ball_color = ntslv[-1]
 #                 for cidx, column in enumerate(pos_of_balls):
 #                     while len(column) > 0 and column[-1] == ball_color and cidx != ntslvidx:
 #                         print("column ", cidx, " to column ", ntslvidx)
 #                         ntslv.append(column[-1])
 #                         del (column[-1])
-#         if len(crpts) > 1:
-#             hptsidx = (color_list.index(ball_color))
-#             del(crpts[hptsidx], color_list[hptsidx])
-#             crpts = balls_points(pos_of_balls, color_list)
-#             ball_color = higst_pts(crpts, color_list)
 #     return pos_of_balls
 
 
+# def sorting_balls(pos_of_balls, mxcllght, color_list):
+#     balls_pts = balls_points(pos_of_balls, color_list)
+
+#     for a in range(len(color_list)):
+#         color_listcp = color_list.copy()
+#         crpts = balls_pts.copy()
+#         ball_color = higst_pts(crpts, color_listcp)
+
+#         for fmidx, fm_col in enumerate(pos_of_balls):
+#             if len(fm_col) > 0 and fm_col[-1] == ball_color:
+#                 for tcidx, to_col in enumerate(pos_of_balls):
+#                     if to_col[-1] == ball_color and tcidx != fmidx:
+#                         print("bjnfgrwsbbhsrgfhjbvfsrhvjfrshjvgjvh")
+
+#                         while mxcllght != len(to_col) and tcidx != fmidx and to_col[-1] == ball_color and fm_col[-1] == ball_color:
+#                             print("column ", fmidx, " to column ", tcidx)
+#                             to_col.append(fm_col[-1])
+#                             del (fm_col[-1])
+
+#         if len(crpts) > 1:
+#             hptsidx = (color_listcp.index(ball_color))
+#             del(crpts[hptsidx], color_listcp[hptsidx])
+
+#     return pos_of_balls
+
+
+# important
+def check_moves(pos_of_balls, mxcllght):
+
+    all_posible_moves = []
+
+    for fmidx, from_col in enumerate(pos_of_balls):
+
+        if column_solved(from_col, mxcllght) != True:
+
+            if len(from_col) > 0:
+                
+                for tcidx, to_col in enumerate(pos_of_balls):
+                    ball_move = []
+
+                    if len(to_col) == 0 or to_col[-1] == from_col[-1]:
+                        if tcidx != fmidx and  len(to_col) < mxcllght:
+                            ball_move.append(fmidx)
+                            ball_move.append(tcidx)
+                            all_posible_moves.append(ball_move)
+    return all_posible_moves
+
+
+def make_move(pos_of_balls, move):            
+    pos_of_balls = copy.deepcopy(pos_of_balls)
+            
+    pos_of_balls[move[1]].append(pos_of_balls[move[0]][-1])
+    del pos_of_balls[move[0]][-1]
+    print("column ", move[0], " to column ", move[1])
+    return pos_of_balls
+                    
+
+
+def make_n_move(pos_of_balls, mxcllght, last_move, count):
+    all_posible_moves = check_moves(pos_of_balls, mxcllght)
+    for move in all_posible_moves:
+        m_puzzle = make_move(pos_of_balls, move)
+        print(m_puzzle)
+        if last_move != None and [last_move[1], last_move[0]] == move:
+            count += 1
+            if count > 5:
+                count = 0
+                break
+                
+        last_move = move
+        if pos_solved(m_puzzle, mxcllght) == True:
+            return m_puzzle
+        
+        if len(check_moves(m_puzzle, mxcllght)) != 0:
+            cp_puzzle = copy.deepcopy(m_puzzle)
+            make_n_move(cp_puzzle, mxcllght, last_move, count)
+
+
+# def count_moves(moves):
+#     count = 0
+#     for move in moves:
+#         for move_2 in moves:
+#             if move == move_2 or [move[1], move[0] == move_2]:
+#                 count += 1
+#                 if count >
+
+
 if __name__ == "__main__":
-    file_name = "kule.txt"
+
+    import copy
+
+    file_name = "data.txt"
+    mod_pos = [['pink', 'lime', 'blue', 'red'], ['pink', 'pink', 'red', 'blue', 'green'], ['lime', 'lime', 'pink', 'green', 'pink'], ['orange', 'purple', 'purple', 'orange', 'blue'], ['blue', 'green', 'light_blue', 'purple', 'gray'], ['green', 'lime', 'green', 'blue', 'orange'], ['light_blue', 'light_blue', 'orange', 'light_blue', 'red'], ['light_blue', 'purple', 'red', 'red', 'lime'], ['gray', 'gray', 'orange', 'purple', 'gray'], ['gray'], []]
+
     pos_of_balls = (ball_pos(file_name))
     print(pos_of_balls)
+
     color_list = balls_colors(pos_of_balls)
-    print(color_list)
+    # print(color_list)
+
     mxcllght = max_col_lght(pos_of_balls, color_list)
-    print(mxcllght)
+    # print(mxcllght)
+
     points = balls_points(pos_of_balls, color_list)
-    print(points)
+    print(f'Column points {points}')
+
     hig_pts = higst_pts(points, color_list)
-    print(hig_pts)
-    empty = empty_col_color(pos_of_balls, mxcllght, color_list)
-    print(empty)
-    # srt_blls = sorting_balls(pos_of_balls, mxcllght, color_list)
-    # print(srt_blls)
     
+
+    possible_moves = check_moves(pos_of_balls, mxcllght)
+    # mod_possible_moves = check_moves(mod_pos, mxcllght)
+    print(f'possible moves: {possible_moves}')
+    # print(f'mod possible moves: {mod_possible_moves}')
+    solve = make_n_move(pos_of_balls, mxcllght, None, 0)
+    print(solve)
+    # print(f'puzzle after moves: {pos_of_balls}')
+    
+    # all_moves = check_moves(pos_of_balls, mxcllght)
+    # mak = make_move(pos_of_balls, mxcllght)
+    # print(mak)
